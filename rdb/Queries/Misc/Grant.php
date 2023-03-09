aa6279c36177342e47ffedb051d5164a84f21d74
Robert Landers <landers.robert@gmail.com>
2023-03-09 23:40:24 +0100

    handle perms better

--- rdb/Queries/Misc/Grant.php
+++ rdb/Queries/Misc/Grant.php
@@ -5,6 +5,7 @@ namespace r\Queries\Misc;
 use r\Datum\BoolDatum;
 use r\Datum\StringDatum;
 use r\Options\GrantOptions;
+use r\Options\GrantPermission;
 use r\ProtocolBuffer\TermTermType;
 use r\Queries\Dbs\Db;
 use r\Queries\Tables\Table;
@@ -12,12 +13,21 @@ use r\ValuedQuery\ValuedQuery;
 
 class Grant extends ValuedQuery
 {
-	public function __construct(Db|Table|null $scope, string $user, GrantOptions $options)
+	public function __construct(Db|Table|null $scope, string $user, ...$permission)
 	{
 		$this->setPositionalArg(0, $scope);
 		$this->setPositionalArg(1, new StringDatum($user));
-		foreach ($options->permissions as $permission => $value) {
-			$this->setOptionalArg($permission, new BoolDatum($value));
+		foreach($permission as $p) {
+			if($p instanceof GrantPermission) {
+				$this->setOptionalArg(match($p) {
+					GrantPermission::Read => 'read',
+					GrantPermission::Write => 'write',
+					GrantPermission::Config => 'config',
+					GrantPermission::Connect => 'connect',
+				}, new BoolDatum(true));
+			} else {
+				$this->setOptionalArg($p, new BoolDatum($permission[$p]));
+			}
 		}
 	}
 
